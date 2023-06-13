8b5a2bdcbb2a825688b74d085cc710aeb9eb46fd
Student <student@epicodus-5.hsd1.or.comcast.net>
2023-06-13 16:27:22 -0700

    create extra functions Co-authored By: Jason Church elijahchurch@gmail.com

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,12 @@
+//Business Logic
+function Ticketdatabase() {
+    this.userticket = {};
+}
+
+Ticketdatabase.prototype.addTicket = function(ticket) {
+    this.userticket[ticket.nameofMovie] = ticket
+}
+
 function Ticket(nameOfMovie, time, age) {
     this.nameOfMovie = nameOfMovie;
     this.time = time;
@@ -7,6 +16,38 @@ function Ticket(nameOfMovie, time, age) {
 
 Ticket.prototype.adjustPrice = function() {
     if (this.age <= 13) {
-        this.price = "7"
-    }
+        this.price = "$7"
+    } 
+    else if (this.age >=65) {
+        this.price = "$9"
+    } 
 };
+
+//User Logic
+let ticketDatabase = new Ticketdatabase();
+
+
+function handleFormSubmission(event) {
+    event.preventDefault();
+    const inputtedMovie = document.querySelector("input#new-movieName").value;
+    const inputtedTime = document.querySelector("input#new-time").value; 
+    const inputtedAge = document.querySelector("input#new-age").value;
+    let newTicket = new Ticket(inputtedMovie, inputtedTime, inputtedAge);
+    ticketDatabase.addTicket(newTicket);
+    document.querySelector("input#new-movieName").value = null;
+    document.querySelector("input#new-time").value = null;
+    document.querySelector("input#new-age").value = null;
+}
+
+
+
+//function displayTicketPrice(event) {
+    //const ticketObject = 
+    //ticketObject.adjustPrice();
+//}
+
+
+window.addEventListener("load", function (){
+    document.querySelector("form#new-ticket").addEventListener("submit", handleFormSubmission);
+});
+
